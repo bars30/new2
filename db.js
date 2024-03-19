f03377db3c9ca23f60760702c14f1d0147438c3f
anna3bars <154560345+anna3bars@users.noreply.github.com>
2024-03-19 22:43:20 +0400

    :d\

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: "postgres://an:NyTvQgoVuXTQwZYLRUlxf24Bhb0HgOd7@dpg-cnst548l6cac73dcbt4g-a.oregon-postgres.render.com/node_postgres_gvsz"
-});
+
+    postgres:"//an:NyTvQgoVuXTQwZYLRUlxf24Bhb0HgOd7@dpg-cnst548l6cac73dcbt4g-a/node_postgres_gvsz"
+   });
 
 module.exports = pool;
 
