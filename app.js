ca394cf91603f12bd986ff1cdfd5c4dfe62abfaf
anna3bars <154560345+anna3bars@users.noreply.github.com>
2024-03-19 20:16:06 +0400

    kj

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
-  
+const db = require('./db')
 const app = express();
-const PORT = 5000;
+const PORT = 5050;
   
 app.get('/', (req, res)=>{
     res.status(200);
     res.send("This is from root");
 });
+
+
   
+app.get('/api/users', async (req, res) => {
+    try {
+        const users = await db.query('SELECT * FROM person');
+        res.json({ message: 'Data retrieval successful', users: users.rows });
+    } catch (err) {
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
 app.listen(PORT, (error) =>{
     if(!error)
         console.log("Server is Successfully Running, and App is listening on port "+ PORT)
