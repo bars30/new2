49337a013249d8fa485b1d4e62c3209f56a94668
anna3bars <154560345+anna3bars@users.noreply.github.com>
2024-03-19 22:19:13 +0400

    adde hosted db

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
- user: "postgres",
+ user: "an",
  password: 'nrv47#n58',
- host: "localhost",
+ host: "dpg-cnst548l6cac73dcbt4g-a",
  port: 5432,
- database: "node_postgres"
+ database: "node_postgres_gvsz",
+ postgres: "//an:NyTvQgoVuXTQwZYLRUlxf24Bhb0HgOd7@dpg-cnst548l6cac73dcbt4g-a/node_postgres_gvsz"
 })
 
 module.exports = pool
