400b949645434af3662a1866dec6330b285f9cb5
anna3bars <154560345+anna3bars@users.noreply.github.com>
2024-03-22 23:34:50 +0400

    added bestselledwatches route

--- app.js
+++ app.js
@@ -31,6 +31,26 @@ app.get('/table', async (req, res) => {
     // Your create table route logic here
 });
 
+
+app.get('/api/users', async (req, res) => {
+    const client = db.getClient(); // Get client instance
+    try {
+        await client.connect(); // Connect to the database
+        const result = await client.query('SELECT * FROM bestselledwatches');
+        res.json({ message: 'Data retrieval successful', watches: result.rows });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    } finally {
+        await client.end(); // Close the connection
+    }
+});
+
+
+
+
+
+
+
 const port = process.env.PORT || 5050;
 
 app.listen(port, (error) => {
