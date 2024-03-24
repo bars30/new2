3186346dc603ab9f7f51592f03095ade52a50c0f
anna3bars <154560345+anna3bars@users.noreply.github.com>
2024-03-24 22:36:16 +0400

    added rolexcolloysterperpetual route

--- app.js
+++ app.js
@@ -133,7 +133,19 @@ app.get('/rolex/coll/cosmographdaytona', async (req, res) => {
     }
 });
 
-
+app.get('/rolex/coll/oysterperpetual', async (req, res) => {
+    const client = db.getClient(); // Get client instance
+    try {
+        await client.connect(); // Connect to the database
+        const result = await client.query(`SELECT * FROM  rolexcollections where id = '6'`);
+        const cont = await client.query('SELECT * FROM rolexcolloysterperpetual');
+        res.json({ message: 'Data retrieval successful', watches: result.rows, cont : cont.rows });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    } finally {
+        await client.end(); // Close the connection
+    }
+});
 
 
 
