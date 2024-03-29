097b93e45232c816474528bab00aaa69aa94cd44
anna3bars <154560345+anna3bars@users.noreply.github.com>
2024-03-29 04:16:10 +0400

    added /cartier/antosde route

--- app.js
+++ app.js
@@ -339,6 +339,20 @@ app.get('/cartier/tank', async (req, res) => {
     }
 });
 
+
+app.get('/cartier/antosde', async (req, res) => {
+    const client = db.getClient(); // Get client instance
+    try {
+        await client.connect(); // Connect to the database
+        const cont = await client.query('SELECT * FROM acrtier_santos_de');
+        res.json({ message: 'Data retrieval successful', cont : cont.rows });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    } finally {
+        await client.end(); // Close the connection
+    }
+});
+
 const port = process.env.PORT || 5050;
 
 app.listen(port, (error) => {
